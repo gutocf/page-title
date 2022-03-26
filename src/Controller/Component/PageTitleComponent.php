cc90effefd0e7beaaa95272678292abc5d8513ea
Augusto Cesar Ferreira <gutocf@gmail.com>
2022-03-26 14:39:34 -0300

    adds fluent interface

--- src/Controller/Component/PageTitleComponent.php
+++ src/Controller/Component/PageTitleComponent.php
@@ -8,49 +8,87 @@ use Cake\Controller\Component;
 use Cake\Collection\Collection;
 
 /**
+ * PageTitle component
+ *
+ * @property \Cake\Collection\Collection $titles
  * @method \App\Controller\AppController getController()
  */
 class PageTitleComponent extends Component
 {
     private Collection $titles;
 
+    /**
+     * @inheritDoc
+     */
     protected $_defaultConfig = [
         'default' => null,
         'separator' => ' / ',
         'var' => 'title',
     ];
 
+    /**
+     * @inheritDoc
+     */
     public function initialize(array $config): void
     {
         $this->reset();
     }
 
+    /**
+     * @inheritDoc
+     */
     public function beforeRender(): void
     {
-        $this->getController()->set($this->getConfig('var'), $this->formatTitle());
+        $var = $this->getConfig('var');
+        $title = $this->getFormattedTitle();
+        $this->getController()->set($var, $title);
     }
 
-    public function add(...$titles): void
+    /**
+     * @param string ...$titles Titles to add
+     * @return self
+     */
+    public function add(string ...$titles): self
     {
         $this->titles = $this->titles->append($titles);
+
+        return $this;
     }
 
-    public function clearAndAdd(...$titles): void
+    /**
+     * Reset titles and add new ones.
+     *
+     * @param string ...$titles Clear current titles and add new ones
+     * @return self
+     */
+    public function clearAndAdd(string ...$titles): self
     {
         $this->reset();
         $this->add(...$titles);
+
+        return $this;
     }
 
-    public function reset(): void
+    /**
+     * Resets the titles collection and adds the default title.
+     *
+     * @return self
+     */
+    public function reset(): self
     {
-        $this->titles = new Collection([]);
+        $this->titles = collection([$this->getConfig('default')]);
+
+        return $this;
     }
 
-    private function formatTitle(): string
+    /**
+     * Returns the formatted title.
+     *
+     * @return string
+     */
+    public function getFormattedTitle(): string
     {
-        $titles = array_reverse($this->titles
-            ->prependItem($this->getConfig('default'))
-            ->toList());
+        $titles = array_reverse($this->titles->toList());
 
         return join($this->getConfig('separator'), $titles);
     }
