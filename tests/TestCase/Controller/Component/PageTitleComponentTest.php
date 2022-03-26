8017dab6e52f6807afdda32f47ff75fb39bf75f9
Augusto Cesar Ferreira <gutocf@gmail.com>
2022-03-26 14:40:40 -0300

    configures phpunit and updates tests

--- tests/TestCase/Controller/Component/PageTitleComponentTest.php
+++ tests/TestCase/Controller/Component/PageTitleComponentTest.php
@@ -6,10 +6,11 @@ use Cake\Http\Response;
 use Cake\Http\ServerRequest;
 use Cake\TestSuite\TestCase;
 use Cake\Controller\Controller;
+use Cake\Event\Event;
 use Gutocf\PageTitle\Controller\Component\PageTitleComponent;
 
 /**
- * @property \App\Controller\Component\PageTitleComponent $component
+ * @property \Gutocf\PageTitle\Controller\Component\PageTitleComponent $PageTitle
  * @property \Cake\Controller\Controller $Controller
  */
 class PageTitleComponentTest extends TestCase
@@ -21,55 +22,104 @@ class PageTitleComponentTest extends TestCase
     {
         parent::setUp();
         $this->Controller = new Controller(new ServerRequest(), new Response());
-        $this->PageTitle = new PageTitleComponent($this->Controller->components());
-        $this->PageTitle->setConfig('default', 'default');
-    }
-
-    public function testAdd()
-    {
-        $this->PageTitle->add('t1');
-        $this->PageTitle->beforeRender();
-        $this->assertEquals('t1 / default', $this->Controller->viewBuilder()->getVar('title'));
-    }
-
-    public function testAddMultiple()
-    {
-        $this->PageTitle->add('t1', 't2');
-        $this->PageTitle->add('t3');
-        $this->PageTitle->beforeRender();
-        $this->assertEquals('t3 / t2 / t1 / default', $this->Controller->viewBuilder()->getVar('title'));
-    }
-
-    public function testChangeSeparator()
-    {
-        $this->PageTitle->setConfig('separator', ' - ');
-        $this->PageTitle->add('t1', 't2');
-        $this->PageTitle->beforeRender();
-        $this->assertEquals('t2 - t1 - default', $this->Controller->viewBuilder()->getVar('title'));
-    }
-
-    public function testClearAndAdd()
-    {
-        $this->PageTitle->add('t1');
-        $this->PageTitle->clearAndAdd('t2');
-        $this->PageTitle->beforeRender();
-        $this->assertEquals('t2 / default', $this->Controller->viewBuilder()->getVar('title'));
-    }
-
-    public function testReset()
-    {
-        $this->PageTitle->add('t1');
-        $this->PageTitle->beforeRender();
-        $this->assertEquals('t1 / default', $this->Controller->viewBuilder()->getVar('title'));
-        $this->PageTitle->reset();
-        $this->PageTitle->add('t2');
-        $this->PageTitle->beforeRender();
-        $this->assertEquals('t2 / default', $this->Controller->viewBuilder()->getVar('title'));
+        $this->PageTitle = new PageTitleComponent($this->Controller->components(), ['default' => 'default']);
     }
 
     public function tearDown(): void
     {
         parent::tearDown();
         unset($this->PageTitle, $this->Controller);
+    }
+
+    public function testAdd()
+    {
+        $this->PageTitle->add('t1');
+        $this->assertEquals('t1 / default', $this->PageTitle->getFormattedTitle());
+    }
+
+    public function testAddMultiple()
+    {
+        $actual = $this->PageTitle
+            ->add('t1', 't2')
+            ->add('t3')
+            ->getFormattedTitle();
+
+        $this->assertEquals('t3 / t2 / t1 / default', $actual);
+
+        $actual = $this->PageTitle
+            ->reset()
+            ->add('t1')
+            ->add('t2', 't3')
+            ->getFormattedTitle();
+
+        $this->assertEquals('t3 / t2 / t1 / default', $actual);
+    }
+
+    public function testChangeSeparator()
+    {
+        $this->PageTitle->setConfig('separator', ' - ');
+        $actual = $this->PageTitle
+            ->add('t1', 't2')
+            ->getFormattedTitle();
+
+        $this->assertEquals('t2 - t1 - default', $actual);
+    }
+
+    public function testClearAndAdd()
+    {
+        $this->PageTitle->add('t1');
+        $actual = $this->PageTitle
+            ->clearAndAdd('t2')
+            ->getFormattedTitle();
+
+        $this->assertEquals('t2 / default', $actual);
+    }
+
+    public function testReset()
+    {
+        $actual = $this->PageTitle
+            ->add('t1')
+            ->getFormattedTitle();
+
+        $this->assertEquals('t1 / default', $actual);
+
+        $actual = $this->PageTitle
+            ->reset()
+            ->add('t2')
+            ->getFormattedTitle();
+
+        $this->assertEquals('t2 / default', $actual);
+    }
+
+    public function testBeforeRender()
+    {
+        $this->PageTitle
+            ->add('t1')
+            ->beforeRender();
+
+        $actual = $this->Controller->viewBuilder()->getVar('title');
+
+        $this->assertEquals('t1 / default', $actual);
+    }
+
+    public function testChangeVar()
+    {
+        $this->PageTitle->setConfig('var', 'my_title');
+        $this->PageTitle
+            ->add('t1')
+            ->beforeRender();
+
+        $actual = $this->Controller->viewBuilder()->getVar('my_title');
+
+        $this->assertEquals('t1 / default', $actual);
+    }
+
+    public function testIntegrationBeforeRender()
+    {
+        $Controller = new Controller(new ServerRequest(), new Response());
+        $Controller->loadComponent('Gutocf/PageTitle.PageTitle', ['default' => 'default']);
+        $Controller->PageTitle->add('t1');
+        $Controller->getEventManager()->dispatch(new Event('Controller.beforeRender', $Controller));
+        $this->assertEquals('t1 / default', $Controller->viewBuilder()->getVar('title'));
     }
 }
